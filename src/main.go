0ed5eb2d47339d42c225c0d92b5be771918763e8
jeftedariel <jeftedariel3@gmail.com>
2024-08-28 00:03:02 -0600

    func to download files from ./Storage

--- src/main.go
+++ src/main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-    "fmt"
+    "log"
     "net/http"
+    "path/filepath"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Looking at: %s\n", r.URL.Path)
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+    //This is to look to the file that will be downloaded
+    filePath := r.URL.Path[len("/download/"):]// And the folder storage which is where we save all the stuff
+    filePath = filepath.Join("storage", filePath)
+
+    // Prevent directory traversal attacks
+    if !strings.HasPrefix(filepath.Clean(filePath), "storage") {
+        http.Error(w, "Invalid file path", http.StatusBadRequest)
+        return
+    }
+    //then if we can find it, we send it back
+    http.ServeFile(w, r, filePath)
 }
 
-func main() {
-    http.HandleFunc("/", handler)
 
-    fmt.Println("Starting server on :8080")
+func main() {
+    http.HandleFunc("/download/", downloadHandler)
+
+    log.Println("Starting server on :8080")
     if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Error starting server:", err)
+        log.Fatal("Error starting server:", err)
     }
 }
 
